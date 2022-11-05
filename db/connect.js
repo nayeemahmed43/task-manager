274b347ddb41231406d8ec33da95dfe78850b81a
nayeem43 <nayeemahmedru@gmail.com>
2022-11-05 23:38:30 +0600

    code refactoring

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
+  
 
-const connectionString =
-  'mongodb+srv://test:Pass@123@cluster0.srshq3z.mongodb.net/task-manager?retryWrites=true&w=majority';
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
